7f11ca7d5e443bcbd9c6557d417db80f2da11bfe
VeenaRagi <163105402+VeenaRagi@users.noreply.github.com>
2024-12-12 15:20:14 +0530

    trying routing

---
+++ app/About.js
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const About=()=>{
+    return(
+        <SafeAreaView>
+            <Text>This is the about page !!!!!!!!!!!!</Text>
+        </SafeAreaView>
+    )
+}
+export default About
--- app/index.js
+++ app/index.js
@@ -1,9 +1,46 @@
-import { View,Text } from "react-native-web";
-const Home=()=>{
-    <view>
-        <Text>
-            Home
-        </Text>
-    </view>
-}
-export default Home
+import {
+    View,
+    Text,
+    ScrollView,
+    SafeAreaView,
+    TouchableOpacity,
+  } from "react-native";
+  import { useState } from "react";
+  import { Stack, useRouter } from "expo-router";
+  
+  import styles from './components/Header/HeaderStyles'
+  const Home = () => {
+    const router = useRouter();
+    const [showText, setShowText] = useState(false);
+    const toggleVisibility = () => {
+      setShowText(!showText);
+    };
+  
+    return (
+      <SafeAreaView style={{ flex: 1, backgroundColor: "pink" }}>
+        <Stack.Screen
+          options={{
+            headerStyle: { backgroundColor: "blue" },
+            headerShadowVisible: false,
+          }}
+        />
+        <Text>Home vnaaa</Text>
+        <View style={styles.container}>
+          <TouchableOpacity onPress={toggleVisibility}>
+            <Text>Click Me</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>Second Btn</Text>
+          </TouchableOpacity>
+        </View>
+        {showText && <Text>This is the text below the btn!!!!!!!!</Text>}
+          <TouchableOpacity onPress={()=>router.push('/about')}>
+            <Text>Go to home</Text>
+          </TouchableOpacity>
+      </SafeAreaView>
+    );
+  };
+  export default Home;
+  
+// app.js/index.js-> this is the main root page
+// app/home.js-> this should be /home page
