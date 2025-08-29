2a2052fdfd602e4f3174683f581b8dc2b9a50bc6
Veena.R <veena.r@egradtutuor.in>
2025-08-29 14:04:06 +0530

    login success haha

---
+++ eGradMobileApp/src/apiConfig.js
@@ -0,0 +1,2 @@
+export const frontEndUrl=`192.168.0.106`
+export const backEndUrl=`http://localhost:5003`;
--- eGradMobileApp/src/screen/LoginScreen.jsx
+++ eGradMobileApp/src/screen/LoginScreen.jsx
@@ -4,22 +4,34 @@ import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-nativ
 export const LoginScreen = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const handleLogin = () => {
+    const handleLogin = async () => {
+         console.log("handleLogin called ✅");
         if (!email || !password) {
             Alert.alert("Validaion Error, Please enter email and password");
-            return
+            return;
         }
+        console.log(password,email,"these r password nd emails");
+        console.log()
         try {
-            
+            const response = await fetch("http://192.168.0.106:5003/login/studentLogin", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    email,password,
+                    instituteOrDomain:"http://192.168.0.106:19000"
+                }),
+            });
+            console.log(response,"this is the responseee");
+
         } catch (error) {
-            
+            console.log(error,"error while login")
         }
     }
     return (
         <View>
             <Text style={styles.title}>LoginScreen</Text>
-            <TextInput style={styles.input} placeholder='Email' value={email} onChange={setEmail} />
-            <TextInput value={password} secureTextEntry onChange={setPassword} style={styles.input} placeholder='Password' />
+            <TextInput style={styles.input} placeholder='Email' value={email} onChangeText={setEmail} />
+            <TextInput value={password} secureTextEntry onChangeText={setPassword} style={styles.input} placeholder='Password' />
             <TouchableOpacity style={styles.qbBtn} onPress={handleLogin}>
                 <Text >
                     Login
