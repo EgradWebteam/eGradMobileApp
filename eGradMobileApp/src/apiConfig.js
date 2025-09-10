9f3c8b6e941c9d2378e0c1aba9966ce8c8e7b4c9
pavani <pavani.n@egradtutor.in>
2025-09-10 14:57:11 +0530

    issue with report page

--- eGradMobileApp/src/apiConfig.js
+++ eGradMobileApp/src/apiConfig.js
@@ -1,3 +1,3 @@
-export const frontEndUrl=`http://192.168.0.106`
-export const backEndUrl=`http://192.168.0.106:5003`;
+export const frontEndUrl=`http://192.168.0.108`
+export const backEndUrl=`http://192.168.0.108:5003`;
 export const backEndPort=`5003`;
