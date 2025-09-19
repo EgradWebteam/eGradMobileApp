90c7e7afc08076cb921a6fd1a434273e3021dcfa
Undela Nandini <nandini.u@egradtutor.in>
2025-09-19 16:44:25 +0530

    when error during session check clear the async storage

--- eGradMobileApp/src/apiConfig.js
+++ eGradMobileApp/src/apiConfig.js
@@ -1,4 +1,4 @@
-export const frontEndUrl=`http://192.168.45.211`
-// export const backEndUrl=`http://192.168.45.211:5003`;
-export const backEndUrl=`https://onlinewebapp-b9audqd8f9e9bfee.centralindia-01.azurewebsites.net`;
+export const frontEndUrl=`http://192.168.134.211`
+export const backEndUrl=`http://192.168.134.211:5003`;
+// export const backEndUrl=`https://onlinewebapp-b9audqd8f9e9bfee.centralindia-01.azurewebsites.net`;
 export const backEndPort=`5003`;
