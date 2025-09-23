0c24361f8f7cfb871607abec6a6e6c7494cce495
Undela Nandini <nandini.u@egradtutor.in>
2025-09-23 10:16:41 +0530

    commit

--- eGradMobileApp/src/apiConfig.js
+++ eGradMobileApp/src/apiConfig.js
@@ -1,4 +1,4 @@
 export const frontEndUrl=`http://192.168.69.28`
-export const backEndUrl=`http://192.168.69.28:5003`;
-// export const backEndUrl=`https://onlinewebapp-b9audqd8f9e9bfee.centralindia-01.azurewebsites.net`;
+// export const backEndUrl=`http://192.168.69.28:5003`;
+export const backEndUrl=`https://onlinewebapp-b9audqd8f9e9bfee.centralindia-01.azurewebsites.net`;
 export const backEndPort=`5003`;
